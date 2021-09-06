c3dcdf531d2b72f65737b56a56896099b8106001
kinley777 <kingzong777@gmail.com>
2021-09-06 09:58:08 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const ip ="127.0.0.1"
+
+const port = 3000;  //choose any above 1024
+
+const server = http.createServer((req,res) =>{
+res.statusCode = 200
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello from server!!!');
+
+});
+
+server.listen(port,ip,() => console.log('server running...'));
+
+
